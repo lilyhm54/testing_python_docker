e42bfe28429dde41a9d11310a83cd54490ac6357
lilymartin <lily.martin5459@gmail.coma>
2020-08-21 17:28:06 +1000

    added framework for main.py

---
+++ app/main.py
@@ -0,0 +1,17 @@
+# main file for running test data analysis 
+
+def main():
+
+    # load data 
+
+    # remove rows with NaN 
+
+    # remove uneeded columns: Name, tikcet, Cabin
+
+    # encode categorical variable: Sex, Embarked 
+
+    # Bucket groups: Age, Fare 
+
+    # Run through model 
+
+    # Return confusion matrix 
